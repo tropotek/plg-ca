fb6745ff41c983f3a733beb19ed6026f0967d660
tropotek <info@tropotek.com>
2022-01-19 11:12:11 +1100

    ~Auto: Commit

---
+++ sql/mysql/00005.sql
@@ -0,0 +1,9 @@
+
+-- --------------------------------------------
+-- @version 3.9.66
+-- @author: Michael Mifsud <info@tropotek.com>
+--
+-- --------------------------------------------
+
+
+INSERT INTO ca_scale

0f99f455b3960e2124d19cfa2a8c00541e3650c0
tropotek <info@tropotek.com>
2022-01-25 05:16:40 +1100

    ~Auto: Commit

--- sql/mysql/00005.sql
+++ sql/mysql/00005.sql
@@ -7,7 +7,7 @@
 
 
 INSERT INTO ca_scale (institution_id, name, description, type, multiple, calc_type, max_value, del, modified, created)
-  VALUES (1, 'Species', 'Provide a list of species the student has worked on during placement', 'choice', 1, 'avg', 3.0, 0, NOW(), NOW())
+  VALUES (1, 'Species', 'Provide a list of species the student has worked on during placement', 'choice', 0, 'avg', 3.0, 0, NOW(), NOW())
 ;
 
 INSERT INTO ca_option (scale_id, name, description, value, del, modified, created) VALUES
@@ -17,5 +17,4 @@ INSERT INTO ca_option (scale_id, name, description, value, del, modified, create
     (LAST_INSERT_ID(), 'Other', 'Exotics and animals that do not fit into the other categories.', 4, 0, NOW(), NOW())
 ;
 
-
-
+-- TODD you need to add the new species to the Assessment forms manually...
